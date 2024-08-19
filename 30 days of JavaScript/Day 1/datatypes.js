13a9d72e46bc72d12b4d85abd9226f9ee2201c09
Kyikyelormor <somek2022@gmail.com>
2024-08-19 21:25:09 +0100

    Day 1

---
+++ 30 days of JavaScript/Day 1/datatypes.js
@@ -0,0 +1,11 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let userNae = "Sumayyah";
+let middleNae;
+let ags = 10;
+let isMarry = false;
+
+console.log(typeof userNae)
+console.log(typeof middleNae)
+console.log(typeof ags)
+console.log(typeof isMarry)
---
+++ 30 days of JavaScript/Day 1/variable.js
@@ -0,0 +1,49 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let first = "Ola" //string
+let isGender = true //boolean
+let greetings;
+let your = null
+
+
+
+// Declare four variables without assigning values
+let user;
+let name;
+let aged;
+let stat;
+
+// Declare four variables with assigned values
+let userName = "Sumayyah";
+let middleName = "Ayomide";
+let ages = 10;
+let status = "single";
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Olaide"
+console.log(firstName)
+
+let lastName = "Muhibudeen"
+console.log(lastName)
+
+let maritalStatus = false
+console.log(maritalStatus)
+
+let country = "Nigeria"
+console.log(country)
+
+let age = 35
+console.log(age)
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 10;
+console.log("I am", myAge, "years old")
+
+let yourAge = 30;
+console.log("You are", yourAge, "years old")
