ccf4687364b4ece5d0e66d42d1b48ebe4534464d
Kyikyelormor <somek2022@gmail.com>
2024-08-26 18:16:10 +0100

    coding challenge

---
+++ 30 days of JavaScript/Day 5/script.js
@@ -0,0 +1,16 @@
+let arr = [];
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  console.log(webTechs.length)
+  console.log(webTechs[0])
+
+  const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+  console.log(ages.sort())
+  console.log(ages.minMax())
