c36be89cad7f57a8e3e7a3d40de6d8e8ad40df76
Kyikyelormor <somek2022@gmail.com>
2024-08-26 16:45:32 +0100

    coding challenge

---
+++ 30 days of JavaScript/Day 2/script.js
@@ -0,0 +1,12 @@
+let challenge = "30 Days Of JavaScript"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(0,1))
+console.log(challenge.split(""))
+console.log(challenge.replace("JavaScript", "Python"))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt("J"))
+console.log(challenge.repeat(2))
+
